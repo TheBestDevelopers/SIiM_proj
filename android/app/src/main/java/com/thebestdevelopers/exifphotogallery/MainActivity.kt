fcf019a675122aae6bf77796ae2344be9b5e3033
Kamil Ismail <ismail.katowice@gmail.com>
2019-03-17 13:09:54 +0100

    Utworzenie struktury projektu

--- android/app/src/main/java/com/thebestdevelopers/exifphotogallery/MainActivity.kt
+++ android/app/src/main/java/com/thebestdevelopers/exifphotogallery/MainActivity.kt
@@ -3,22 +3,32 @@ package com.thebestdevelopers.exifphotogallery
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.thebestdevelopers.exifphotogallery.fragments.gallery.GalleryFragment
+import com.thebestdevelopers.exifphotogallery.fragments.search.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_gallery -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.mainFrame, GalleryFragment.newInstance(), getString(R.string.gallery_tag))
+                    .addToBackStack(null)
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            R.id.navigation_search -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.mainFrame, SearchFragment.newInstance(), getString(R.string.search_tag))
+                    .addToBackStack(null)
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_camera -> {
+                launchCamera()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +40,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mainFrame, GalleryFragment.newInstance(), getString(R.string.gallery_tag))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun launchCamera() {
+
     }
 }
